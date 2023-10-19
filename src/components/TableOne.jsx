b0292ad8fe8ac6082838d652586263043ca48f0f
Saksham Sharma <saksham555sharma@gmail.com>
2023-10-19 21:41:21 +0530

    changes done 19 oct

--- src/components/TableOne.jsx
+++ src/components/TableOne.jsx
@@ -3,10 +3,25 @@ import { NavLink } from "react-router-dom";
 import baseUrl from "../Config";
 import toast, { Toaster } from 'react-hot-toast';
 
+const LIMIT = 10;
+
+const totalPagesCalculator = (total, limit) => {
+  const pages = [];
+  for(let x = 1; x<= parseInt(total)/limit; x++){
+    pages.push(x);
+  }
+
+  return pages;
+}
+
 const TableOne = () => {
   const [reviews, setReviews] = useState([]);
   const [change, setChange] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [activePage, setActivePage] = useState(1);
+const [totalUsers, setTotalUsers] = useState(0);
+
+  
 
 
   useEffect(() => {
@@ -16,6 +31,7 @@ const TableOne = () => {
         result.json().then((resp) => {
           console.log("result", resp)
           setReviews(resp.reviews)
+          setTotalUsers(data.total);
           console.log(resp.reviews)
         })
       })
@@ -244,6 +260,29 @@ const TableOne = () => {
             </div>))
 
           }
+
+          
+<nav aria-label="Page navigation example">
+  <ul class="inline-flex -space-x-px text-sm">
+  {activePage !== 1 &&
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={() => setActivePage(activePage - 1)}>Previous</a>
+    </li>
+}
+{totalPagesCalculator(totalUsers, LIMIT).map(page => (
+    <li>
+      <a href="#" class={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${activePage === page ? 'active' : ''} `}   onClick={() => setActivePage(page)}>{page}</a>
+    </li>
+
+))}
+    {activePage !== totalPagesCalculator(totalUsers, LIMIT).length &&
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={() => setActivePage(activePage + 1)}>Next</a>
+    </li> }
+  </ul>
+</nav>
+
+
 
 
         </div>
